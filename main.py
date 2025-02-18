43e6d19f02364836af101a3231b80c7f6d5575d2
DdraigX <6390496+DdraigX@users.noreply.github.com>
2025-02-18 12:27:46 -0500

    update

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import os
 import textwrap
 
 
-def clear_screen()
     
 
 def get_terminal_size(): 
@@ -14,8 +13,8 @@ def draw_box(rows, columns):
     #for i in range(columns):
     #print(f"{'\u2500' * columns}")
     print(f"\033[{2};0H{'¦'}{' ' * (columns - 2)}{'¦'}")
-    print(f"{"─" * columns}")
-    "─"
+    print(f"{'┌' + '─' * (columns - 2) + '┐'}")
+  
     print(f"{'|'}")
     print("\033[1;31mThis text is red and bold!\033[0m")
 
